9fc4117bda64e97980e829deb9255cd2f5d2307d
FESS <dreamer.alex.toms@gmail.com>
2019-05-04 22:07:05 +0300

    test picasso and circle images

--- app/src/main/java/com/example/testgson/MainActivity.kt
+++ app/src/main/java/com/example/testgson/MainActivity.kt
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +15,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        recyclerView_main.setBackgroundColor(Color.BLUE)
 
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+//        recyclerView_main.adapter = MainAdapter()
 
+        fetchJson()
+    }
 
-        recyclerView_main.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
-        recyclerView_main.adapter = MainAdapter()
+    fun fetchJson() {
+        println("Attempting to Fetch JSON")
+
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+
+                runOnUiThread {
+                    recyclerView_main.adapter = MainAdapter(homeFeed)
+                }
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute request")
+            }
+        })
     }
 }
+
--- app/src/main/java/com/example/testgson/MainAdapter.kt
+++ app/src/main/java/com/example/testgson/MainAdapter.kt
@@ -4,33 +4,42 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+
 import kotlinx.android.synthetic.main.video_row.view.*
 
-class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
 
-    val videoTitles = arrayListOf("123","234","345","456")
+    val videoTitles = listOf("First title", "Second", "3rd", "MOOOOORE TITLE")
 
+    // numberOfItems
     override fun getItemCount(): Int {
-        return videoTitles.size
+        return homeFeed.videos.count()
     }
 
-    override fun onBindViewHolder(p0: CustomViewHolder, p1: Int) {
-        val videoTitle = videoTitles.get(p1)
-        p0?.view?.textView_video_title?.text = videoTitle
-
-    }
-
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
+    override fun onCreateViewHolder(p0: ViewGroup, viewType: Int): CustomViewHolder {
+        // how do we even create a view
         val layoutInflater = LayoutInflater.from(p0?.context)
-        val cellForRow = layoutInflater.inflate(R.layout.video_row,p0,false)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, p0, false)
         return CustomViewHolder(cellForRow)
-
     }
+
+    override fun onBindViewHolder(p0: CustomViewHolder, position: Int) {
+//        val videoTitle = videoTitles.get(position)
+        val video = homeFeed.videos.get(position)
+        p0?.view?.textView_video_title?.text = video.name
+
+        p0?.view?.textView_channel_name?.text = video.channel.name + "  •  " + "20K Views\n4 days ago"
+
+        val thumbnailImageView = p0?.view?.imageView_video_thumbnail
+        Picasso.get().load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImageView = p0?.view?.imageView_channel_profile
+        Picasso.get().load(video.channel.profileImageUrl).into(channelProfileImageView)
+    }
+
 }
 
-
-
-
-class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
 
 }
